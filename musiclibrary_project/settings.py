669dd3991d046d6beab2a6db287ec6ada6f18965
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-04-07 13:26:26 -0500

    Removed key from settings that was left there due to operator error

--- musiclibrary_project/settings.py
+++ musiclibrary_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*uny3@(-y9gx@)*f&onr*#f_ih016d9^$9cis@h**n@zsr499f'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
